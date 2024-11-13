01e6621c053c0a060f3d3429eb7e9e03021b876f
Abhinav Kourav <78190083+abhinav-2021@users.noreply.github.com>
2024-11-14 05:03:14 +0530

    Update form.js

--- form.js
+++ form.js
@@ -1,9 +1,14 @@
     // Initialize EmailJS with your public key
     emailjs.init("1nHhaMAcfeGDaQBkV"); // Replace with your actual public key
-  
+
+    const loadingOverlay = document.getElementById("loading-overlay");
+    const form = document.getElementById("contact-form");
+
     // Form submission event
     document.getElementById("contact-form").addEventListener("submit", function(event) {
-  event.preventDefault(); // Prevent the form from refreshing the page
+      event.preventDefault(); // Prevent the form from refreshing the page
+      loadingOverlay.style.display = 'flex'; // Show the loading overlay with blur
+
 
   // Send email using EmailJS
   emailjs.sendForm("service_bm43you", "template_zxuh5ws", this)
@@ -14,6 +19,7 @@
       messageDiv.textContent = "Your message was sent successfully!";
       messageDiv.className = "form-message success";
       messageDiv.style.display = "block";
+      loadingOverlay.style.display = 'none'; // Hide the loading overlay
 
       // Hide the message after 3 seconds
       // setTimeout(function() {
@@ -23,6 +29,7 @@
 
       // Reset the form after submission
       document.getElementById("contact-form").reset();
+
     }, function(error) {
       console.log('FAILED...', error);
       // Show error message
@@ -30,6 +37,7 @@
       messageDiv.textContent = "There was an error sending your message. Please try again.";
       messageDiv.className = "form-message error";
       messageDiv.style.display = "block";
+      loadingOverlay.style.display = 'none'; // Hide the loading overlay
     });
 
     // Hide the message after 3 seconds
